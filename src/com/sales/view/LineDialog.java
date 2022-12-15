666579a0daec3b72bdcabf3a862b842502c37f68
ahmedhegazy225 <megadiab84@gmail.com>
2022-12-15 22:45:07 +0200

    Initial commit

---
+++ src/com/sales/view/LineDialog.java
@@ -0,0 +1,63 @@
+package com.sales.view;
+
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+
+public class LineDialog extends JDialog{
+    private JTextField itemNameField;
+    private JTextField itemCountField;
+    private JTextField itemPriceField;
+    private JLabel itemNameLabel;
+    private JLabel itemCountLabel;
+    private JLabel itemPriceLabel;
+    private JButton okButton;
+    private JButton cancelButton;
+    
+    public LineDialog(InvoiceGui frame) {
+        itemNameField = new JTextField(20);
+        itemNameLabel = new JLabel("Item Name");
+        
+        itemCountField = new JTextField(20);
+        itemCountLabel = new JLabel("Item Count");
+        
+        itemPriceField = new JTextField(20);
+        itemPriceLabel = new JLabel("Item Price");
+        
+        okButton = new JButton("OK");
+        cancelButton = new JButton("Cancel");
+        
+        okButton.setActionCommand("createLineOK");
+        cancelButton.setActionCommand("createLineCancel");
+        
+        okButton.addActionListener(frame.getController());
+        cancelButton.addActionListener(frame.getController());
+        setLayout(new GridLayout(4, 2));
+        
+        add(itemNameLabel);
+        add(itemNameField);
+        add(itemCountLabel);
+        add(itemCountField);
+        add(itemPriceLabel);
+        add(itemPriceField);
+        add(okButton);
+        add(cancelButton);
+        
+        pack();
+    }
+
+    public JTextField getItemNameField() {
+        return itemNameField;
+    }
+
+    public JTextField getItemCountField() {
+        return itemCountField;
+    }
+
+    public JTextField getItemPriceField() {
+        return itemPriceField;
+    }
+}
